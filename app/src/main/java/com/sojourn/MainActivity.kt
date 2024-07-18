10998199abf9eb110c6174b812d183546ea1ab50
JeisunDev <56722713+Jason-Karcheski@users.noreply.github.com>
2024-07-18 17:01:37 +0100

    Add SojournTextField

--- app/src/main/java/com/sojourn/MainActivity.kt
+++ app/src/main/java/com/sojourn/MainActivity.kt
@@ -9,7 +9,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import com.sojourn.common.feature.composable.SojournTextField
 import com.sojourn.common.feature.composable.SojournTopBar
 import com.sojourn.common.feature.theme.SojournTheme
 import com.sojourn.common.feature.theme.sojournColors
@@ -19,6 +24,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+            var textFieldContent by remember { mutableStateOf("") }
+
             SojournTheme {
                 Surface(color = MaterialTheme.sojournColors.background) {
                     Column(modifier = Modifier.systemBarsPadding().fillMaxSize()) {
@@ -27,6 +35,12 @@ class MainActivity : ComponentActivity() {
                             subtitle = "A Subtitle",
                             onBackPressed = {},
                             isCollapsed = false
+                        )
+
+                        SojournTextField(
+                            value = textFieldContent,
+                            onValueChanged = { new -> textFieldContent = new },
+                            placeholder = "Example Placeholder"
                         )
                     }
                 }
